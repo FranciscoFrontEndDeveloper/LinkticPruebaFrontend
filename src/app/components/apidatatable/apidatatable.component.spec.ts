c9202047932c1aafabdb13493cb120347fde1a5c
FRANCISCO <pachonetworking@gmail.com>
2025-05-10 15:51:53 -0500

    data table functional

---
+++ src/app/components/apidatatable/apidatatable.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ApidatatableComponent } from './apidatatable.component';
+
+describe('ApidatatableComponent', () => {
+  let component: ApidatatableComponent;
+  let fixture: ComponentFixture<ApidatatableComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ApidatatableComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ApidatatableComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
