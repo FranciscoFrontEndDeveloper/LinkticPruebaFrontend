efde0d5d746b855c6490276d4c9841586eb17bbc
FRANCISCO <pachonetworking@gmail.com>
2025-05-11 18:48:48 -0500

    maincomponent complemented styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { ApimenuComponent } from "./components/apimenu/apimenu.component";
-import { MaincomponentComponent } from "./components/maincomponent/maincomponent.component";
 import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ApimenuComponent, MatSlideToggleModule, MaincomponentComponent,RouterModule],
+  imports: [RouterOutlet, MatSlideToggleModule,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass',
   standalone: true,
