fdb33de00d8fbda0f9dcd5d814d60f39b4290fba
FRANCISCO <pachonetworking@gmail.com>
2025-05-11 13:58:24 -0500

    maincomponent test succesful

--- src/app/components/maincomponent/maincomponent.component.spec.ts
+++ src/app/components/maincomponent/maincomponent.component.spec.ts
@@ -1,23 +1,62 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 import { MaincomponentComponent } from './maincomponent.component';
-
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 describe('MaincomponentComponent', () => {
   let component: MaincomponentComponent;
   let fixture: ComponentFixture<MaincomponentComponent>;
 
+  TestBed.configureTestingModule({
+  imports: [ MaincomponentComponent ],
+
+})
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MaincomponentComponent]
-    })
-    .compileComponents();
+      imports: [MaincomponentComponent],
+      providers: [
+      {
+        provide: ActivatedRoute,
+        useValue: {
+          params: of({}), 
+          snapshot: {
+            paramMap: {
+              get: () => null
+            }
+          }
+        }
+      }
+    ],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MaincomponentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('El componente se crea', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Se debe mostrar el titulo', () => {
+  const compiled = fixture.nativeElement as HTMLElement;
+  expect(compiled.querySelector('h1')?.textContent).toContain('master of apis');
+  });
+  
+  it('La ruta al siguiente componente con routerLink a "/apimenu"', () => {
+  const compiled = fixture.nativeElement as HTMLElement;
+  const button = compiled.querySelector('button[routerLink="/apimenu"]');
+  expect(button).toBeTruthy();
+  });
+  
+  it('debe mostrar el texto "start" y el ícono "login" en el botón', () => {
+  const compiled = fixture.nativeElement as HTMLElement;
+  const button = compiled.querySelector('button[routerLink="/apimenu"]');
+  const icon = button?.querySelector('mat-icon');
+
+  expect(button?.textContent?.toLowerCase()).toContain('start');
+  expect(icon?.textContent?.trim()).toBe('login');
+});
 });
--- src/app/services/jsonplaceholder.service.spec.ts
+++ src/app/services/jsonplaceholder.service.spec.ts
@@ -1,16 +1,43 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  provideHttpClientTesting,
+  HttpTestingController,
+   
+} from '@angular/common/http/testing';
 import { JsonplaceholderService } from './jsonplaceholder.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 
 describe('JsonplaceholderService', () => {
   let service: JsonplaceholderService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], 
+      providers: [JsonplaceholderService, provideHttpClientTesting(),],
+    });
     service = TestBed.inject(JsonplaceholderService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('En teoria devuelme datos simulados', () => {
+    const dataFake = [
+      { id: 1, title: 'api info test 1' },
+      { id: 2, title: 'api info test 2' },
+    ];
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    service.getApiData(url).subscribe((data) => {
+      expect(data).toEqual(dataFake);
+    });
+
+    const requestApi = httpMock.expectOne(url);
+    expect(requestApi.request.method).toBe('GET');
+    requestApi.flush(dataFake)
     expect(service).toBeTruthy();
   });
 });
