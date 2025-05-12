6955688ebab0d6d17819e1058ffb8e9422110960
FRANCISCO <pachonetworking@gmail.com>
2025-05-11 21:13:14 -0500

    api menu styles modificated

--- src/app/components/apimenu/apimenu.component.ts
+++ src/app/components/apimenu/apimenu.component.ts
@@ -2,11 +2,9 @@ import { Component, } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { GenericviewComponent } from "../genericview/genericview.component";
 import { ApifilterComponent } from "../apifilter/apifilter.component";
-import { ApidatatableComponent } from "../apidatatable/apidatatable.component";
-import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-apimenu',
-  imports: [MatSidenavModule, GenericviewComponent, ApifilterComponent, ApidatatableComponent,RouterOutlet],
+  imports: [MatSidenavModule, GenericviewComponent, ApifilterComponent, ],
   templateUrl: './apimenu.component.html',
   styleUrl: './apimenu.component.sass',
   standalone: true
